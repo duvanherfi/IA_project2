0fde987b221680aa27168750b4698eb7b53e0b35
Duvan Hernandez Figueroa <duvanherfi@gmail.com>
2022-12-30 10:25:23 -0500

    Change interfaz with menu

--- Interfaz.py
+++ Interfaz.py
@@ -100,13 +100,12 @@ class Ventana:
 class Tablero(Ventana):
     DIFICULTAD = [2, 4, 6]
 
-    def __init__(self, grid=[], pasos=[], paso_actual=0, nivel=1, largo=50, alto=50, margen=5):
+    def __init__(self, grid=[], largo=50, alto=50, margen=5):
         # grid
-        self.grid = pasos[paso_actual] if len(pasos) > 0 else grid
+        self.grid = np.zeros([8, 8], dtype=int)
         self.grid_inicial = self.grid
-        self.pasos = pasos
         # nivel
-        self.nivel = nivel
+        self.nivel = None
         # largo y alto
         self.ancho = largo
         self.alto = alto
@@ -117,39 +116,33 @@ class Tablero(Ventana):
         margen_total = ((self.margen * 10) + 5)
         # inicializamos pygame
         super().__init__(largo_m * margen_total, (alto_m * margen_total)+100)
-        self.paso_actual = paso_actual
         self.caballo = None
         self.bonus = None
         self.inicializar_imagenes()
         self.turno = 1
         self.fin = False
-        self.profundidad = self.DIFICULTAD[self.nivel]
+        self.profundidad = None
         self.time_init = time.time()
         self.jugadas_1 = []
         self.jugadas_2 = []
-        self.pos_ant = 0
         self.muestra = False
         self.calcular = False
+        x = (self.dimension_ventana[0] / 2)
+        y = 100
+        self.titulo = Fuente('Gabriola', 50, "War Horses", (x - 7 * 10, y))
+        self.principiante = Fuente(
+            'Gabriola', 34, "principiante", (x - 5.5 * 10, y + 100))
+        self.amateur = Fuente('Gabriola', 34, "amateur",
+                              (x - 3.5 * 10, y + 170))
+        self.experto = Fuente('Gabriola', 34, "experto",
+                              (x - 3.5 * 10, y + 240))
+        self.menu = True
 
     def inicializar_imagenes(self):
         self.caballo = pygame.image.load("img/caballo.png").convert()
         self.caballo.set_colorkey(self.NEGRO)
         self.bonus = pygame.image.load("img/bonus.png").convert()
         self.bonus.set_colorkey(self.NEGRO)
-
-    def set_pos_ant(self):
-        pos_ant = list(map(lambda x: x[0], np.where(self.grid == 2)))
-        self.grid[pos_ant[0]][pos_ant[1]] = 0
-
-    def update_pos_pasos(self, paso):
-        op = self.paso_actual + paso
-        if op >= len(self.pasos):
-            self.paso_actual = 0
-            self.mostrar = False
-        elif op < 0:
-            self.paso_actual = len(self.pasos) - 1
-        else:
-            self.paso_actual = op
 
     def pos_mouse(self):
         pos = pygame.mouse.get_pos()
@@ -161,29 +154,26 @@ class Tablero(Ventana):
     def loop_events(self):
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
-                self.salir = True
+                self.grid = np.zeros([8, 8], dtype=int)
+                self.grid_inicial = self.grid
+                self.turno = 1
+                self.fin = False
+                self.profundidad = None
+                self.jugadas_1 = []
+                self.jugadas_2 = []
+                self.muestra = False
+                self.calcular = False
+                self.menu = True
             if evento.type == pygame.MOUSEBUTTONDOWN:
                 if evento.button == 1 and self.turno == 2:
                     mov = list(
                         filter(lambda x: (x[self.pos_mouse()] == 1), self.jugadas_2))
                     if len(mov) > 0:
-                        # print("Turno 2")
-                        # print(f"pos mouse: {self.pos_mouse()}")
                         self.grid = mov[0]
                         self.grid_inicial = self.grid
-                        # print(self.grid)
-                        self.pasos.append(self.grid)
-                        self.paso_actual = len(self.pasos) - 1
                         self.turno = 1
                         self.time_init = time.time()
                         self.jugadas_2 = []
-
-            if evento.type == pygame.KEYDOWN:
-                if len(self.pasos) > 0:
-                    if evento.key == pygame.K_RIGHT:
-                        self.update_pos_pasos(1)
-                    if evento.key == pygame.K_LEFT:
-                        self.update_pos_pasos(-1)
 
     def dibujar_imagen(self, imagen, columna, fila):
         self.pantalla.blit(
@@ -193,13 +183,6 @@ class Tablero(Ventana):
                 ((self.margen + self.alto) * fila + self.margen) + 100
             ]
         )
-
-    def set_grid(self):
-        if len(self.pasos) > 0:
-            if self.paso_actual == 0:
-                self.grid = self.pasos[0]
-            else:
-                self.grid = self.pasos[self.paso_actual]
 
     def jugadasj(self):
         return np.count_nonzero(np.array(self.grid_inicial) == 4) + 1
@@ -244,8 +227,6 @@ class Tablero(Ventana):
         )
 
     def draw_loop(self):
-        # Dibujamos la retícula
-        self.set_grid()
         mov = list(
             filter(lambda x: (x[self.pos_mouse()] == 1), self.jugadas_2))
 
@@ -297,21 +278,12 @@ class Tablero(Ventana):
             delay = 0.3 if len(self.jugadas_2) == 1 else 1.5
             if _time <= delay:
                 return
-            # print("turno 1")
             nodo = Nodo(self.grid, 1)
             self.jugadas_1 = Juego().crearArbol(self.profundidad, [nodo])
-            # print("jugadas 1")
-            # print(self.jugadas_1)
-            # print(len(self.jugadas_1))
             if len(self.jugadas_1) > 1:
-                self.pasos.append(self.grid)
-                # print(self.grid)
                 jugadas = list(np.copy(self.jugadas_1))
                 self.grid = Juego().recorrerMinimax(jugadas, self.profundidad).entorno
                 self.grid_inicial = self.grid
-                # print(self.grid)
-                self.pasos.append(self.grid)
-                self.paso_actual = len(self.pasos) - 1
             self.turno = 2
             self.time_init = time.time()
 
@@ -323,62 +295,41 @@ class Tablero(Ventana):
     def main_loop(self):
         # -------- Bucle Principal del Programa-----------
         while not self.salir:
-            self.loop_events()
+            if self.menu:
+                self.menu_loop()
+            else:
+                self.loop_events()
+                if self.menu:
+                    continue
+                self.reloj.tick(60)
 
-            self.reloj.tick(60)
+                # Establecemos el fondo de pantalla.
+                self.pantalla.fill(self.NEGRO)
 
-            # Establecemos el fondo de pantalla.
-            self.pantalla.fill(self.NEGRO)
+                self.turno_maq()
+                self.draw_loop()
 
-            self.turno_maq()
-            self.draw_loop()
+                self.calcular_mov_jug()
 
-            self.calcular_mov_jug()
+                self.draw_resumen()
 
-            self.draw_resumen()
+                if self.fin:
+                    self.draw_ganador()
 
-            if self.fin:
-                self.draw_ganador()
+                # actualizar rectangulo de cursor
+                self.cursor.updatecursor()
 
-            # actualizar rectangulo de cursor
-            self.cursor.updatecursor()
-
-            # Avanzamos y actualizamos la pantalla con lo que hemos dibujado.
-            pygame.display.flip()
+                # Avanzamos y actualizamos la pantalla con lo que hemos dibujado.
+                pygame.display.flip()
 
         # Pórtate bien con el IDLE.
         pygame.quit()
-        Menu().show_window()
 
     def show_window(self):
         self.main_loop()
 
-
-class Menu(Ventana):
-
-    def __init__(self, largo=600, alto=400):
-        # largo y alto
-        self.largo = largo
-        self.alto = alto
-        # inicializamos pygame
-        super().__init__(largo, alto)
-
-        x = (self.dimension_ventana[0]/2)
-        y = 20
-        self.titulo = Fuente('Gabriola', 50, "War Horses", (x - 7 * 10, y))
-        self.principiante = Fuente(
-            'Gabriola', 34, "principiante", (x - 5.5 * 10, y + 100))
-        self.amateur = Fuente('Gabriola', 34, "amateur",
-                              (x - 3.5 * 10, y + 170))
-        self.experto = Fuente('Gabriola', 34, "experto",
-                              (x - 3.5 * 10, y + 240))
-        self.grid = None
-        self.chars = None
-        self.nivel = None
-
     def iniciar_nivel(self):
         random.seed(a=time.time(), version=2)
-        self.grid = np.zeros([8, 8], dtype=int)
         _range = range(0, 8)
         caballo1 = np.array(random.sample(_range, 2))
         caballo2 = np.empty([1])
@@ -403,75 +354,67 @@ class Menu(Ventana):
                     np.array_equal(bono3, caballo1) is False and \
                     np.array_equal(bono3, caballo2) is False:
                 break
-        self.chars = [[caballo1, 1], [caballo2, 2],
+        chars = [[caballo1, 1], [caballo2, 2],
                  [bono, 3], [bono2, 3], [bono3, 3]]
-        for char in self.chars:
+        for char in chars:
             self.grid[char[0][0]][char[0][1]] = char[1]
         #grid = np.loadtxt('entorno.txt', dtype=int)
-        gc.collect()
-        Tablero(grid=self.grid, nivel=self.nivel).show_window()
+        self.profundidad = self.DIFICULTAD[self.nivel]
+        self.time_init = time.time()
 
-
-    def loop_events(self):
+    def loop_events_menu(self):
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
-                sys.exit(0)
+                if self.menu:
+                    sys.exit(0)
             if evento.type == pygame.MOUSEBUTTONDOWN:
                 if evento.button == 1:
                     if self.cursor.colliderect(self.principiante.rect):
                         self.nivel = 0
                         self.iniciar_nivel()
-                        self.salir = True
+                        self.menu=False
                     if self.cursor.colliderect(self.amateur.rect):
                         self.nivel = 1
                         self.iniciar_nivel()
-                        self.salir = True
+                        self.menu = False
                     if self.cursor.colliderect(self.experto.rect):
                         self.nivel = 2
                         self.iniciar_nivel()
-                        self.salir = True
+                        self.menu = False
 
-    def main_loop(self):
-        # -------- Bucle Principal del Programa-----------
-        while not self.salir:
-            self.loop_events()
+    def menu_loop(self):
+        self.loop_events_menu()
 
-            self.reloj.tick(60)
+        self.reloj.tick(60)
 
-            # Establecemos el fondo de pantalla.
-            self.pantalla.fill(self.NEGRO)
+        # Establecemos el fondo de pantalla.
+        self.pantalla.fill(self.NEGRO)
 
-            self.pantalla.blit(
-                self.titulo.render(),
-                self.titulo.coor
-            )
+        self.pantalla.blit(
+            self.titulo.render(),
+            self.titulo.coor
+        )
 
-            self.pantalla.blit(
-                self.principiante.render(cursor=self.cursor),
-                self.principiante.coor
-            )
+        self.pantalla.blit(
+            self.principiante.render(cursor=self.cursor),
+            self.principiante.coor
+        )
 
-            self.pantalla.blit(
-                self.amateur.render(cursor=self.cursor),
-                self.amateur.coor
-            )
+        self.pantalla.blit(
+            self.amateur.render(cursor=self.cursor),
+            self.amateur.coor
+        )
 
-            self.pantalla.blit(
-                self.experto.render(cursor=self.cursor),
-                self.experto.coor
-            )
+        self.pantalla.blit(
+            self.experto.render(cursor=self.cursor),
+            self.experto.coor
+        )
 
-            # actualizar rectangulo de cursor
-            self.cursor.updatecursor()
+        # actualizar rectangulo de cursor
+        self.cursor.updatecursor()
 
-            # Avanzamos y actualizamos la pantalla con lo que hemos dibujado.
-            pygame.display.flip()
-
-        # Pórtate bien con el IDLE.
-        pygame.quit()
-
-    def show_window(self):
-        self.main_loop()
+        # Avanzamos y actualizamos la pantalla con lo que hemos dibujado.
+        pygame.display.flip()
 
 
-Menu().show_window()
+Tablero().show_window()
